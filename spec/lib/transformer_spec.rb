999c7a9541f4e85fb6863dbf2c838524b9988c39
Ahmad Tolba <tolpa1@gmail.com>
2021-08-09 00:20:23 +0200

    Add spec for error raising

--- spec/lib/transformer_spec.rb
+++ spec/lib/transformer_spec.rb
@@ -6,12 +6,19 @@ RSpec.describe Transformer do
   subject { described_class }
 
   let(:input) { JSON.parse({"username":"xyz","upload":"xyz"}.to_json) }
+  let(:invalid_input) { JSON.parse({"username":"xyz","upload":"xyz","echoed":true}.to_json) }
   let(:output) { { "username":"xyz","upload":"xyz","echoed":true } }
 
   context "# Transformer" do
     it "transofrm json and adds echoed field" do
       transformed = subject.new(input).transform
       expect(transformed.to_json).to eq output.to_json
+    end
+  end
+
+  context "# Transformer Error" do
+    it "raise an error if echoed field exists" do
+      expect { subject.new(invalid_input).transform }.to raise_error
     end
   end
 end
